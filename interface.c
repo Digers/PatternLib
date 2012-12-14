10f0dd636057d8de5389b8f7af2ddcab0555923a
Daniel Digerås <developer@netile.se>
2012-12-14 13:24:57 +0100

    Fixed license

--- interface.c
+++ interface.c
@@ -1,12 +1,12 @@
 /*
 PatternLib Copyright (C) 2012  Daniel Digerås
 
-Foobar is free software: you can redistribute it and/or modify
+PatternLib is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+PatternLib is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser Public License for more details.
